4ed153c2c40ace3dd5a94d3ce51cf6a654fb96cb
João Vitor da Silva Marques <jvsilvam@outlook.com>
2021-10-17 15:04:55 -0300

    Mission update17

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -64,24 +64,26 @@ class MissionController extends Controller
 
                 $array['data'] = $completeMission;
             } else {
-                if($mis[0]{"complete"} === false) {
-                    InfoMission::where(['id_user' => $id_user, 'id_mission' => $id])
-                        ->update(['complete' => true]);
+                // if($mis[0]{"complete"} === false) {
+                //     InfoMission::where(['id_user' => $id_user, 'id_mission' => $id])
+                //         ->update(['complete' => true]);
             
-                    $award = Mission::select('award_value')->where('id', $id)->get();
-                    $gc = $award[0]{"award_value"};
+                //     $award = Mission::select('award_value')->where('id', $id)->get();
+                //     $gc = $award[0]{"award_value"};
 
-                    $newGc = GreenCredit::find($this->loggedUser->id);
-                    $newGc->amountgc = $newGc->amountgc + $gc;
-                    $newGc->save();
+                //     $newGc = GreenCredit::find($this->loggedUser->id);
+                //     $newGc->amountgc = $newGc->amountgc + $gc;
+                //     $newGc->save();
                 
-                    $completeMission = InfoMission::select()->where(['id_user' => $id_user, 'id_mission' => $id])->get();
+                //     $completeMission = InfoMission::select()->where(['id_user' => $id_user, 'id_mission' => $id])->get();
 
-                    $array['data'] = $completeMission;
-                } else {
-                    $array['error'] = 'Missão já completada';
-                    return $array;
-                }
+                //     $array['data'] = $completeMission;
+                // } else {
+                //     $array['error'] = 'Missão já completada';
+                //     return $array;
+                // }
+
+                $array['data'] = $mis[0]{"complete"};
             }
         } else {
             $array['error'] = 'Missão não existente';
