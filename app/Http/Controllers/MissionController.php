392a88799ad46d75aa51ab1e36e4081aa00d0c19
João Vitor da Silva Marques <jvsilvam@outlook.com>
2021-10-17 15:01:30 -0300

    Mission update15

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -43,7 +43,7 @@ class MissionController extends Controller
 
         if($mission) {
             $mis = InfoMission::select()->where(['id_user' => $id_user, 'id_mission' => $id])->get();
-            if(!$mis) {
+            if($mis) {
                 if($mis[0]{"complete"} === false) {
                     InfoMission::where(['id_user' => $id_user, 'id_mission' => $id])
                         ->update(['complete' => true]);
@@ -81,7 +81,7 @@ class MissionController extends Controller
             
                 $completeMission = InfoMission::select()->where(['id_user' => $id_user, 'id_mission' => $id])->get();
 
-                $array['data'] = $completeMission[0]{"complete"};
+                $array['data'] = $completeMission;
             }
         } else {
             $array['error'] = 'Missão não existente';
